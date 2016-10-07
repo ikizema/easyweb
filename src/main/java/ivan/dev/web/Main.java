69de13514bda145baa69d10ccfb7834cdf8ebe8c
ikizema <kizema.ivan@gmail.com>
2016-10-07 00:12:00 -0400

    Configure Logback with Groovy

--- src/main/java/ivan/dev/web/Main.java
+++ src/main/java/ivan/dev/web/Main.java
@@ -1,9 +1,13 @@
 package ivan.dev.web;
 
+import static org.kohsuke.args4j.ExampleMode.ALL;
 import org.eclipse.jetty.server.Server;
 import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -14,13 +18,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    @Option(name="-p",usage="port number", metaVar="PORT")
+    @Option(name="-p",usage="port number", metaVar="PORT", required=false)
     private int port = 8080;
 
     @Argument
     private List<String> arguments = new ArrayList<String>();
     
     ApplicationContext context;
+    
+    Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {    	
     	new Main().parseArgs(args);
@@ -28,8 +34,15 @@ public class Main {
     
     private void parseArgs(String[] args) throws Exception {
     	CmdLineParser parser = new CmdLineParser(this);
-    	parser.parseArgument(args);
-    	
+    	try {
+    		parser.parseArgument(args);
+        } catch( CmdLineException e ) {
+            System.err.println(e.getMessage());
+            System.err.println("java SampleMain [options...] arguments...");
+            parser.printUsage(System.err);
+            return;
+        }
+
     	loadSpringContext();
     	startJettyService();
     }
---
+++ src/main/java/logback.groovy
@@ -0,0 +1,26 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+import static ch.qos.logback.classic.Level.DEBUG
+import static ch.qos.logback.classic.Level.INFO
+import static ch.qos.logback.classic.Level.WARN
+import static ch.qos.logback.classic.Level.ERROR
+import static ch.qos.logback.classic.Level.OFF
+
+appender("STDOUT", ConsoleAppender) {
+  encoder(PatternLayoutEncoder) {
+    pattern = "%d{HH:mm:ss.SSS} [%thread] [%-5level] [%logger{6}] : %msg%n"
+  }
+}
+root(INFO, ["STDOUT"])
+//logger("ivan.dev.web", DEBUG, ["STDOUT"])
+//logger("ivan.dev.web", WARN, ["STDOUT"])
+
+//Append to file
+//def bySecond = timestamp("yyyyMMdd'T'HHmmss")
+//appender("FILE", FileAppender) {
+//  file = "./log/log-${bySecond}.log"
+//  append = true
+//  encoder(PatternLayoutEncoder) {
+//    pattern = "%level %logger - %msg%n"
+//  }
+//}
