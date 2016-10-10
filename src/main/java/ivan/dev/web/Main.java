54532a8c0c7e3ad5dbcdf77834fd3b03b61d1f10
ikizema <kizema.ivan@gmail.com>
2016-10-10 15:39:47 -0400

    Parse CLI to Spring Properties

--- src/main/java/ivan/dev/web/Main.java
+++ src/main/java/ivan/dev/web/Main.java
@@ -5,46 +5,55 @@ import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.io.support.ResourcePropertySource;
+import ivan.dev.web.config.AppConfig;
 import ivan.dev.web.config.model.RuntimeConfig;
 
-public class Main {   
-    ApplicationContext context;
-    RuntimeConfig runtimeConfig;
-    
-    Logger logger = LoggerFactory.getLogger(Main.class);
+public class Main {
+	
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) throws Exception { 	
-    	new Main().run(args);
-    }
-    
-    private void run(String[] args) throws Exception {
-    	loadSpringContext();
-    	
-    	CmdLineParser parser = new CmdLineParser(this.runtimeConfig);
-    	try {
-    		parser.parseArgument(args);
-        } catch( CmdLineException e ) {
-            System.err.println(e.getMessage());
-            parser.printUsage(System.err);
-            return;
-        }
+    public static void main(String[] args) throws Exception {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
 
-    	startJettyService();
-    }
-    
-    private void loadSpringContext() {
-    	this.context = new ClassPathXmlApplicationContext(new String[] {"classpath:/ivan/dev/web/config/spring-context.xml"});
-//    	this.context = new AnnotationConfigApplicationContext(AppConfig.class);
-    	
-    	this.runtimeConfig=context.getBean(RuntimeConfig.class);
-    }
-    
-    private void startJettyService() throws Exception {
-    	Server server = new Server(runtimeConfig.getPort());
-        server.setHandler(context.getBean(JettyService.class));
-        server.start();
-        server.join();
+        // Parse Command Line Arguments
+        RuntimeConfig runtimeConfig = new RuntimeConfig();
+		CmdLineParser parser = new CmdLineParser(runtimeConfig);
+		try {
+			parser.parseArgument(args);
+	    } catch( CmdLineException e ) {
+	        System.err.println(e.getMessage());
+	        parser.printUsage(System.err);
+	        return;
+	    }
+
+		// Pass CLI arguments to Spring Context
+		ConfigurableEnvironment env = context.getEnvironment();
+		MutablePropertySources propertySources = env.getPropertySources();
+		propertySources.addFirst(new MapPropertySource("cli", runtimeConfig.getProperties()));
+		
+		String envProperties = "classpath:/ivan/dev/web/config/env/env-${env}.properties";
+		envProperties = envProperties.replace("${env}", env.getProperty("env"));
+		ResourcePropertySource envPropertiesRessources = new ResourcePropertySource("envProperties", envProperties);
+		propertySources.addFirst(envPropertiesRessources);
+		
+        logger.info("Port : {}", runtimeConfig.getPort());
+        logger.info("Env : {}", runtimeConfig.getEnv());
+        logger.info("Property project : {}", env.getProperty("project"));
+        logger.info("Property env : {}", env.getProperty("env"));
+        logger.info("Property port : {}", env.getProperty("port"));
+        logger.info("Property topic : {}", env.getProperty("topic"));
+        logger.info("Property server : {}", env.getProperty("server"));
+
+    	Server server = new Server(Integer.parseInt(env.getProperty("port")));
+    	server.setHandler(context.getBean(JettyService.class));
+    	server.start();
+    	server.join();
     }
 }
--- src/main/java/ivan/dev/web/config/model/RuntimeConfig.java
+++ src/main/java/ivan/dev/web/config/model/RuntimeConfig.java
@@ -1,12 +1,20 @@
 package ivan.dev.web.config.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.Option;
 
 public class RuntimeConfig {
+	
+	public static enum Env {DEV, QA, UAT, PROD}
+	
+    @Option(name="-e",usage="Environnement [DEV, QA, UAT, PROD]", metaVar="ENV", required=false)
+    private Env env = Env.DEV;
+	
     @Option(name="-p",usage="port number", metaVar="PORT", required=false)
     private int port = 8080;
 
@@ -17,15 +25,18 @@ public class RuntimeConfig {
 		return port;
 	}
 
-	public void setPort(int port) {
-		this.port = port;
-	}
-
 	public List<String> getArguments() {
 		return arguments;
 	}
 
-	public void setArguments(List<String> arguments) {
-		this.arguments = arguments;
+	public Env getEnv() {
+		return env;
+	}
+	
+	public Map<String, Object> getProperties() {
+		Map<String,Object> properties = new HashMap<String, Object>();
+		properties.put("env", env);
+		properties.put("port", port);
+		return properties;
 	}
 }
